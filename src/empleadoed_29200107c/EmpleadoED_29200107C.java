0c705a85326bfa27580792fcc4978e23ba90f83e
Angeles Dobon <angelesdobon@gmail.com>
2021-01-14 10:29:11 +0100

    Versión final con Javadoc DNI 29200107C

--- src/empleadoed_29200107c/EmpleadoED_29200107C.java
+++ src/empleadoed_29200107c/EmpleadoED_29200107C.java
@@ -5,18 +5,37 @@
  */
 package empleadoed_29200107c;
 
+/**
+ * EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
+ * 
+ * @author ÁNGELES DOBÓN PÉREZ DNI 29200107C
+ * @version 1.0
+ */
 public class EmpleadoED_29200107C {
-
+  
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
-    public EmpleadoED_29200107C(String nombreCompleto, String dni) {
+  /**
+   *Constructor con tres parámetros
+   * 
+   * @param nombreCompleto nombre completo en mayúsculas
+   * @param dni documento nacional de identidad con letra y sin espacios
+   */
+  public EmpleadoED_29200107C(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
 
-    public double getSalarioEsteMes(int horasExtra) {
+  /**
+   *Calcula el salario bruto mensual en función del salario base por hora y las horas extras
+   * El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA
+   * 
+   * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+   * @return devuelve el salario bruto mensual en euros con dos decimales
+   */
+  public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
         salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
@@ -25,6 +44,7 @@ public class EmpleadoED_29200107C {
     }
 
     // Este método no lo comentaremos con JavaDoc
+
     public static void main(String[] args) {
         // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
         EmpleadoED_29200107C emp = new EmpleadoED_29200107C("ANGELES DOBÓN", "29200107c");
